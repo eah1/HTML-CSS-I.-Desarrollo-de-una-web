642f8229317f6ba42d8b8a2cf9a58af0e50a23d4
eah1 <eduardoarnedo90@gmail.com>
2021-12-07 19:53:37 +0100

    Add responsive images

--- src/js/category.js
+++ src/js/category.js
@@ -5,6 +5,10 @@ let categories = [
         "title": "Experimenta la ingravidez",
         "description": "La empresa Zero G ofrece vuelos suborbitales en los que podrás experimentar la ingravidez. Para lograrlo utilizan un Boeing 727 modificado que vuela en arcos parabólicos. La caída en picado permite disfrutar de 20-30 segundos sin peso durante unas 15 veces.",
         "image": require("../img/experienceWeightlessness.jpg"),
+        "image200": require("../img/experienceWeightlessness_200.jpg"),
+        "image400": require("../img/experienceWeightlessness_400.jpg"),
+        "image600": require("../img/experienceWeightlessness_600.jpg"),
+        "image800": require("../img/experienceWeightlessness_800.jpg"),
         "alt": "Turistas en el A310"
     },
     {
@@ -12,6 +16,10 @@ let categories = [
         "title": "Vuelos suborbitales",
         "description": "Existen aviones que, aunque no lleguen hasta el espacio, sí vuelan lo suficientemente alto como para percibir la curvatura de la Tierra. Diversas empresas rusas ofrecen vuelos supersónicos para civiles. Para ello, utilizan aviones MIG-29, uno d elos pocos jets que pueden acelerar verticalmente como los cohetes.",
         "image": require("../img/suborbitalFlights.png"),
+        "image200": require("../img/suborbitalFlights_200.png"),
+        "image400": require("../img/suborbitalFlights_400.png"),
+        "image600": require("../img/suborbitalFlights_600.png"),
+        "image800": require("../img/suborbitalFlights_800.png"),
         "alt": "Piloto del MIG-2"
     },
     {
@@ -19,6 +27,10 @@ let categories = [
         "title": "Vuelos espaciales de Virgen Galactic",
         "description": "La compañía estadounidense Virgen Galactic del magnate Richard Branson, ofrece vuelos suborbitales que prometen alcanzar el límite del espacio. Este límite se encuentra a unos 100 km de altura en lo que se conoce como la línea de Kármán.",
         "image": require("../img/virginGalactic.jpg"),
+        "image200": require("../img/virginGalactic_200.jpg"),
+        "image400": require("../img/virginGalactic_400.jpg"),
+        "image600": require("../img/virginGalactic_600.jpg"),
+        "image800": require("../img/virginGalactic_800.jpg"),
         "alt": "Aeronave Virgin Galactic"
     },
     {
@@ -26,11 +38,15 @@ let categories = [
         "title": "Visita la Estación Espacial",
         "description": "Más de 200 personas han visitado la Estación Espacial Internacional hasta la fecha. Sin embargo, solo 7 civiles afortunados (y adinerados) han cumplido este sueño.",
         "image": require("../img/spaceStation.jpg"),
+        "image200": require("../img/spaceStation_200.jpg"),
+        "image400": require("../img/spaceStation_400.jpg"),
+        "image600": require("../img/spaceStation_600.jpg"),
+        "image800": require("../img/spaceStation_800.jpg"),
         "alt": "Estación espacial"
     }
 ]
 
-categories.forEach((category) =>{
+categories.forEach((category) => {
     renderCategory(category);
 });
 
@@ -44,17 +60,19 @@ function renderCategory(category) {
     let categoryImage = createElement("img", "multimedia");
     categoryImage.src = category.image;
     categoryImage.alt = category.alt;
+    categoryImage.srcset = category.image200 + " 200w," + category.image400 + " 400w," + category.image600 + " 600w," + category.image800 + " 800w"
+    categoryImage.sizes = "(max-width:320px) 180px, (max-width:480px) 230px, (max-width:800px) 380px, (max-width:1024px) 590px, 650px"
     divImage.append(categoryImage)
     categoryPage.append(divImage)
 
-    let categoryDescription= createElement("p", null, category.description)
+    let categoryDescription = createElement("p", null, category.description)
     categoryPage.append(categoryDescription);
 
     let categoryLinkP = createElement("p");
 
     let categoryLink = createElement("a", "links");
     categoryLink.href = `${category.fileHTML}.html`;
-    categoryLink.innerHTML  = 'Más información';
+    categoryLink.innerHTML = 'Más información';
 
     categoryLinkP.append(categoryLink)
     categoryPage.append(categoryLinkP)
